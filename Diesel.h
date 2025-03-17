60fe71c28d1ab393bd934373b51c6e97c1b0b4c1
art-diesel <antonneizbeznyj@gmail.com>
2025-03-18 01:26:50 +0300

    First problems colution

---
+++ Diesel.h
@@ -0,0 +1,6 @@
+void EnterSize(int32_t&);
+bool CheckSize(int32_t );
+void FillArray(double* ,int32_t );
+int32_t MaxDigit(double* ,int32_t );
+int32_t MinDigit(double* ,int32_t );
+double  CalculateAverage(double* ,int32_t );
